e8f0b160397f32011a0c982dc841fa4d36c1454e
Jaycee <jaycee.lock@gmail.com>
2019-06-11 11:01:24 +0100

    Initial implementation of whycon detector, need to integrate Java and JNI code

--- app/src/main/cpp/include/MarkerDetector/MarkerDetector.hpp
+++ app/src/main/cpp/include/MarkerDetector/MarkerDetector.hpp
@@ -2,6 +2,8 @@
 #include <whycon/imageproc/CTransformation.h>
 #include <whycon/common/CRawImage.h>
 
+#include <cstring>
+
 #define MAX_IDS 16
 
 namespace MarkerDetector
@@ -14,8 +16,14 @@ namespace MarkerDetector
         bool init();
         bool kill();
 
+        void processImage(unsigned char* data);
+
     private:
         CCircleDetect* patternDetectors[MAX_IDS];
+        SSegment currentSegmentArray[MAX_IDS];
+        SSegment lastSegmentArray[MAX_IDS];
+        STrackedObject objectArray[MAX_IDS];
+
         CTransformation* trans;
         CRawImage* image;
 
--- app/src/main/cpp/src/MarkerDetector/MarkerDetector.cpp
+++ app/src/main/cpp/src/MarkerDetector/MarkerDetector.cpp
@@ -7,6 +7,67 @@ namespace MarkerDetector
     bool MarkerDetector::init()
     {
         trans = new CTransformation(imageWidth, imageHeight, circleDiameter, true);
+        trans->transformType = TRANSFORM_NONE;
+
+        image = new CRawImage(imageWidth, imageHeight);
+
+        for(int i = 0; i < MAX_IDS; i ++)
+        {
+            patternDetectors[i] = new CCircleDetect(imageWidth, imageHeight, i);
+        }
+
         return 0;
+    }
+
+    bool MarkerDetector::kill()
+    {
+        delete image;
+        delete trans;
+
+        for(int i = 0; i < MAX_IDS; i ++)
+        {
+            delete patternDetectors[i];
+        }
+
+        return 0;
+    }
+
+    void MarkerDetector::processImage(unsigned char *data)
+    {
+        // Copy in new image data
+        memcpy(image->data, data, sizeof(image->data));
+
+        // Track markers found in previous search
+        for(int i = 0; i < MAX_IDS; i ++)
+        {
+            if(currentSegmentArray[i].valid)
+            {
+                lastSegmentArray[i] = currentSegmentArray[i];
+                currentSegmentArray[i] = patternDetectors[i]->findSegment(image, lastSegmentArray[i]);
+            }
+        }
+
+        // Find untracked markers
+        for(int i = 0; i < MAX_IDS; i++)
+        {
+            if(!currentSegmentArray[i].valid)
+            {
+                lastSegmentArray[i].valid = false;
+                currentSegmentArray[i] = patternDetectors[i]->findSegment(image, lastSegmentArray[i]);
+            }
+            if(!currentSegmentArray[i].valid) break;
+        }
+
+        // Perform coordinate transformation
+        for(int i = 0; i < MAX_IDS; i++)
+        {
+            if(currentSegmentArray[i].valid)
+            {
+                if(currentSegmentArray[i].valid)
+                {
+                    objectArray[i] = trans->transform(currentSegmentArray[i], false);
+                }
+            }
+        }
     }
 }
